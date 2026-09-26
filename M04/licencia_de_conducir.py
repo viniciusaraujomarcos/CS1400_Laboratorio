6c06e362f7fff56e4af5c91fad07a839c4b8daf3
malvesdearaujo@mail.weber.edu <malvesdearaujo@mail.weber.edu>
2026-09-25 19:18:27 -0600

    starting the program

--- M04/licencia_de_conducir.py
+++ M04/licencia_de_conducir.py
@@ -3,17 +3,26 @@
 TODO
 Crea un programa interactivo que evalúe si una persona mayor de edad está en
 condiciones de conducir. Usa como referencia lo visto en la M3 Tarea de Sentencias.
+#
 Requisitos:
 Entrada de datos: Solicita la edad del usuario y al menos 2 o 3 condiciones
  adicionales.
+ #
 Sentencias de control: Usa estructuras condicionales (if, else if, else)
  y operadores lógicos (AND, OR, NOT) para evaluar la combinación de datos.
+ #
 Salida clara: Muestra un mensaje personalizado indicando si la persona puede
  conducir o si debe entregar las llaves inmediatamente.
+ #
 ¡Usa tu creatividad! 
  Piensa en situaciones cómicas o extremas de la vida real.
    ¿Qué imprudencia o descuido no le permitirías a tu abuela antes de subirse al auto?
      (Ejemplo: "¿Olvidó los lentes en la cocina?")
 """
-#
-# segundo texte
+# empezando.
+print("¡Buenas tardes!")
+print("Esto es solo un procedimiento de rutina...")
+edad = int(input("¿Cuántos años tienes? "))
+if edad < 18:
+    print("Llama a algún adulto para que te lleve a casa; todavía no puedes conducir..")
+else:
